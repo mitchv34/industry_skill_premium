e2358a1d42e31e982d5bd8ebc23075219ffd880d
mitchvb34 <valdsbobes@wisc.edu>
2022-08-28 22:44:57 -0500

    update

--- estimation/runfile2.jl
+++ estimation/runfile2.jl
@@ -25,7 +25,7 @@ inds_done = [f[1:end-4] for f in readdir("data/results/ind_est")]
 
 indx_ = [i for i in 1:length(inds.code_klems) if ~(inds.code_klems[i] in inds_done)]
 
-i = indx_[7]
+i = indx_[end]
 codes[i]
 begin
 	ind_proc = readdir("data/results/ind_est")
@@ -54,7 +54,7 @@ delta_e = mean(dataframe.DPR_EQ)
 delta_s = mean(dataframe.DPR_ST)
 
 ### Set initial parameter values
-scale_initial = 10.0
+scale_initial = 3.0
 η_ω_0 = 0.001
 param_0 = [0.11, .5, -.5] 
 
